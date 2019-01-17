330bcbb89e5d9e8004c6c073c87cfa760cf0612f
MetalDogL <lilkavanilka@gmail.com>
2019-01-17 17:37:46 +0100

    hardest task so far is done now

---
+++ greenfox/week-01/day-4/swap.ts
@@ -0,0 +1,17 @@
+'use strict';
+export {}
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+let c: number = null;
+
+c = a;
+a = b;
+b = c;
+
+
+
+
+console.log(a);
+console.log(b);
