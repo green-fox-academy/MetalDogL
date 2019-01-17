293ed90df2aa05f7c623a6e21916f95dd334ae08
MetalDogL <lilkavanilka@gmail.com>
2019-01-17 15:38:43 +0100

    greeting others

---
+++ greenfox/week-01/day-4/hello-others.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+console.log("Hello, Levente!"); // Hello, Esther!
+console.log("Hello, Maci!"); // Hello, Mary!
+console.log("Hello, Kitti!"); // Hello, Joe!
