35779c50069fac2f29bf7235ecd15c31b5f30fe0
MetalDogL <lilkavanilka@gmail.com>
2019-01-18 14:28:12 +0100

    adding definebasicinfo and bmi

---
+++ week-01/day-4/definebasicinfo.ts
@@ -0,0 +1,22 @@
+'use strict';
+export {}
+
+// Define several things as a variable, then print their values
+
+// Your name as a string
+let name: string = 'Lili';
+console.log(name);
+
+// Your age as an integer
+let myAge: number = 34;
+console.log(myAge);
+
+// Your height in meters as a float
+let height: number = 1.80;
+console.log(height);
+
+// Whether you are married or not as a boolean
+let marriageStatus: boolean = false;
+console.log(marriageStatus);
+
+
